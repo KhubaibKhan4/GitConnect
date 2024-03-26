2133938af4545e7d37b942e266cb8502481662ab
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-03-26 17:12:48 +0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+rootProject.name = "GitConnect"
+include(":composeApp")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
