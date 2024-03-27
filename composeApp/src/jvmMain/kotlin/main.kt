f93e36f5cce117f19bb455806901b72c6a919d46
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-03-27 10:40:16 +0500

    -App Module Impl (#21)

--- composeApp/src/jvmMain/kotlin/main.kt
+++ composeApp/src/jvmMain/kotlin/main.kt
@@ -4,8 +4,13 @@ import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import java.awt.Dimension
 import org.company.app.App
+import org.company.app.di.appModule
+import org.koin.core.context.startKoin
 
 fun main() = application {
+    startKoin {
+        modules(appModule)
+    }
     Window(
         title = "GitConnect",
         state = rememberWindowState(width = 800.dp, height = 600.dp),
