a290dd6a69bb081048f796b5f50764f80ec4d5b4
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-03-27 11:09:10 +0500

    -Tab Items Impl (#27)

--- composeApp/src/commonMain/kotlin/org/company/app/App.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/App.kt
@@ -1,8 +1,21 @@
 package org.company.app
 
-import androidx.compose.animation.core.*
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Notifications
@@ -12,15 +25,42 @@ import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
-import gitconnect.composeapp.generated.resources.*
+import androidx.compose.ui.unit.sp
+import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabNavigator
+import gitconnect.composeapp.generated.resources.IndieFlower_Regular
+import gitconnect.composeapp.generated.resources.Res
+import gitconnect.composeapp.generated.resources.cyclone
+import gitconnect.composeapp.generated.resources.ic_cyclone
+import gitconnect.composeapp.generated.resources.ic_dark_mode
+import gitconnect.composeapp.generated.resources.ic_light_mode
+import gitconnect.composeapp.generated.resources.ic_rotate_right
+import gitconnect.composeapp.generated.resources.open_github
+import gitconnect.composeapp.generated.resources.run
+import gitconnect.composeapp.generated.resources.stop
+import gitconnect.composeapp.generated.resources.theme
 import org.company.app.presentation.navigation.model.NavigationItem
 import org.company.app.theme.AppTheme
 import org.company.app.theme.LocalThemeIsDark
@@ -127,6 +167,41 @@ fun AppContent() {
             Text(stringResource(Res.string.open_github))
         }
     }
+}
+
+@Composable
+fun RowScope.TabItem(tab: Tab) {
+    val tabNavigator = LocalTabNavigator.current
+    NavigationBarItem(
+        selected = tabNavigator.current == tab,
+        onClick = {
+            tabNavigator.current = tab
+        },
+        icon = {
+            tab.options.icon?.let { painter ->
+                Icon(
+                    painter = painter,
+                    contentDescription = null
+                )
+            }
+        },
+        label = {
+            tab.options.title.let { title ->
+                Text(
+                    title,
+                    fontSize = 12.sp
+                )
+            }
+        },
+        enabled = true,
+        alwaysShowLabel = true,
+        colors = NavigationBarItemDefaults.colors(
+            selectedIconColor = Color.Red,
+            unselectedIconColor = Color.Black,
+            selectedTextColor = Color.Red,
+            unselectedTextColor = Color.Black
+        )
+    )
 }
 
 internal expect fun openUrl(url: String?)
