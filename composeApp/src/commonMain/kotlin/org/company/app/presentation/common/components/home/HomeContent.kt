f017a60e42b82b8fa34f4cdaf0da6c52df965b19
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-03-27 12:54:45 +0500

    -Github Users Item Impl (#44)

--- composeApp/src/commonMain/kotlin/org/company/app/presentation/common/components/home/HomeContent.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/presentation/common/components/home/HomeContent.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
@@ -58,11 +59,7 @@ fun HomeContent(
             modifier = Modifier
                 .fillMaxSize()
                 .windowInsetsPadding(WindowInsets.safeDrawing)
-                .padding(
-                    top = it.calculateTopPadding(),
-                    bottom = it.calculateBottomPadding(),
-                    start = 16.dp
-                ),
+                .padding(start = 16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             usersData?.let { UserList(it) }
