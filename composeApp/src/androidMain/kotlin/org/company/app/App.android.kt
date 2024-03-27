508cd07498d858df9957427e68c3404da6ec6e7e
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-03-27 10:19:34 +0500

    -Koin App Module Impl (#16)

--- composeApp/src/androidMain/kotlin/org/company/app/App.android.kt
+++ composeApp/src/androidMain/kotlin/org/company/app/App.android.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import org.koin.core.context.startKoin
 
 class AndroidApp : Application() {
     companion object {
