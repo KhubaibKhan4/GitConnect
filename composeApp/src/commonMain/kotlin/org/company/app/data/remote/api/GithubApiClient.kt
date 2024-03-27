380c2b7aedc82d582bb61073046fa34f06401d4f
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-03-27 09:59:15 +0500

    -Logger Enhancement (#10)

--- composeApp/src/commonMain/kotlin/org/company/app/data/remote/api/GithubApiClient.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/data/remote/api/GithubApiClient.kt
@@ -4,6 +4,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
@@ -21,7 +22,7 @@ object GithubApiClient {
         }
         install(Logging) {
             level = LogLevel.ALL
-            logger = object : io.ktor.client.plugins.logging.Logger {
+            logger = object : Logger {
                 override fun log(message: String) {
                     println(message)
                 }
