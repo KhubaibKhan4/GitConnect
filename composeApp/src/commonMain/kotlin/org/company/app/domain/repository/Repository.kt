6086f267712065c4446a0cc2336504905150cd95
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-03-27 10:07:31 +0500

    -Repository Impl with Github API (#13)

---
+++ composeApp/src/commonMain/kotlin/org/company/app/domain/repository/Repository.kt
@@ -0,0 +1,11 @@
+package org.company.app.domain.repository
+
+import org.company.app.data.remote.api.GithubApiClient
+import org.company.app.data.repository.GithubApi
+import org.company.app.domain.model.UsersItem
+
+class Repository: GithubApi {
+    override suspend fun getAllUsers(): List<UsersItem> {
+        return GithubApiClient.getAllUsers()
+    }
+}
