d5d4cc447910e36b302d8ad76574c7315fa485ee
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-03-27 10:37:45 +0500

    -Koin Module Impl (#19)

--- composeApp/src/iosMain/kotlin/main.kt
+++ composeApp/src/iosMain/kotlin/main.kt
@@ -1,5 +1,11 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import org.company.app.App
+import org.company.app.di.appModule
 import platform.UIKit.UIViewController
 
 fun MainViewController(): UIViewController = ComposeUIViewController { App() }
+fun startKoin(){
+    org.koin.core.context.startKoin {
+        modules(appModule)
+    }
+}
