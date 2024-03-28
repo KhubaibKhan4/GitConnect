7fac11cd063a395dac7643b02dc8637d13ff99ca
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-03-28 11:45:45 +0500

    -Users Details Impl (#57)

--- composeApp/src/commonMain/kotlin/org/company/app/data/repository/GithubApi.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/data/repository/GithubApi.kt
@@ -1,8 +1,9 @@
 package org.company.app.data.repository
 
 import org.company.app.domain.model.UsersItem
+import org.company.app.domain.model.details.UserDetail
 
 interface GithubApi {
     suspend fun getAllUsers(): List<UsersItem>
-    suspend fun getFollowers(username: String): UsersItem
+    suspend fun getFollowers(username: String): UserDetail
 }
--- composeApp/src/commonMain/kotlin/org/company/app/domain/repository/Repository.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/domain/repository/Repository.kt
@@ -3,13 +3,14 @@ package org.company.app.domain.repository
 import org.company.app.data.remote.api.GithubApiClient
 import org.company.app.data.repository.GithubApi
 import org.company.app.domain.model.UsersItem
+import org.company.app.domain.model.details.UserDetail
 
-class Repository: GithubApi {
+class Repository : GithubApi {
     override suspend fun getAllUsers(): List<UsersItem> {
         return GithubApiClient.getAllUsers()
     }
 
-    override suspend fun getFollowers(username: String): List<UsersItem> {
+    override suspend fun getFollowers(username: String): UserDetail {
         return GithubApiClient.getFollowers(username)
     }
 }
