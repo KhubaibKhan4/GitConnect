3e5bf5386f2b5907d392d6cc75eee0e8f0844517
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-03-28 10:24:50 +0500

    -Get Followers Repository Impl (#49)

--- composeApp/src/commonMain/kotlin/org/company/app/domain/repository/Repository.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/domain/repository/Repository.kt
@@ -8,4 +8,8 @@ class Repository: GithubApi {
     override suspend fun getAllUsers(): List<UsersItem> {
         return GithubApiClient.getAllUsers()
     }
+
+    override suspend fun getFollowers(username: String): List<UsersItem> {
+        return GithubApiClient.getFollowers(username)
+    }
 }
